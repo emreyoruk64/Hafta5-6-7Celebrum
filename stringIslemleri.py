9dc7403d5cdc975b13d1085484a55de8b87c54d2
emreyoruk <emreyoruk6464@gmail.com>
2025-03-11 15:50:27 +0300

    Delete stringIslemleri.py

--- stringIslemleri.py
+++
@@ -1,7 +0,0 @@
-girdi=str(input("Lütfen bir cümle giriniz:"))
-print(girdi.upper())
-
-alisverisListesi=["elma","armut","karpuz","kivi","erik"]
-alisverisListesi.append("kavun")
-alisverisListesi.remove("karpuz")
-print(alisverisListesi)
